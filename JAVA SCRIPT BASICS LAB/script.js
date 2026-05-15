0817dd2d8905001679b907b22f60c657a9626c1e
Michael Merrera <spideymikey08@gmail.com>
2026-05-15 17:28:56 +0800

    Add files via upload

---
+++ JAVA SCRIPT BASICS LAB/script.js
@@ -0,0 +1,52 @@
+var name = "Lek";
+let age = 23;
+const country = "Philippines";
+
+console.log("---- subtask 2.1 ----");
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
+
+let numberExample = 100;
+let stringExample = "Hello Lek";
+let booleanExample = true;
+let arrayExample = [1, 2, 3, 4];
+let objectExample = {
+    brand: "Ferrari",
+    model: "488 Pista",
+    year: 2025
+};
+
+console.log("---- subtask 3.1 ----");
+console.log(numberExample);
+console.log(stringExample);
+console.log(booleanExample);
+console.log(arrayExample);
+console.log(objectExample);
+
+console.log(typeof numberExample);   
+console.log(typeof stringExample);   
+console.log(typeof booleanExample);   
+console.log(typeof arrayExample);     
+console.log(typeof objectExample);    
+
+let a = 3;
+let b = 18;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+console.log(a > b);   
+console.log(a < b);  
+console.log(a != b);  
+console.log(a == b);  
+
+let agecheck = 20;
+let isAdult = agecheck >= 18;
+let hasID = true;
+
+console.log("Can enter:", isAdult && hasID);
+console.log("Can enter (either condition):", isAdult || hasID);
+console.log("Not adult:", !isAdult);
